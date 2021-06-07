a88e7335f2eab4146f51f22dbb2cfc3dab53b541
Ana Laura Fernandes de Oliveira <ana.oliveira@4mti.com.br>
2021-06-06 22:40:54 -0300

    Continuação

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,29 @@
+import { useHistory } from 'react-router-dom'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>
+    const history = useHistory()
+
+    function addMeetupHandler(meetupData){
+        fetch('https://curso-react-5c185-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() =>{
+            history.replace('/')
+        })
+    }
+
+    return (
+        <section>
+            <h1>Add a Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
+    )
 }
 
 export default NewMeetupPage
